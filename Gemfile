4f1dc35fa5e62e0cbffbb36c3224a248e925ede1
its-dgreen <dylan@ctrlaltdylan.com>
2021-05-09 16:19:19 -0500

    feat: installed tailwind for UI

--- Gemfile
+++ Gemfile
@@ -58,3 +58,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem "hotwire-rails", "~> 0.1.3"
 
 gem "devise", "~> 4.8"
+
+gem "tailwindcss-rails", "~> 0.3.3"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_09_211215) do
+ActiveRecord::Schema.define(version: 2021_05_09_211237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 2021_05_09_211215) do
     t.index ["user_id"], name: "index_grocery_lists_on_user_id"
   end
 
+  create_table "user_grocery_lists", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "grocery_list_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["grocery_list_id"], name: "index_user_grocery_lists_on_grocery_list_id"
+    t.index ["user_id"], name: "index_user_grocery_lists_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -50,4 +59,6 @@ ActiveRecord::Schema.define(version: 2021_05_09_211215) do
   add_foreign_key "grocery_items", "grocery_lists"
   add_foreign_key "grocery_items", "users"
   add_foreign_key "grocery_lists", "users"
+  add_foreign_key "user_grocery_lists", "grocery_lists"
+  add_foreign_key "user_grocery_lists", "users"
 end
